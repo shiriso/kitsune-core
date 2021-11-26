06514691e57cad8f75aa74f104622da08da05d67
Shiriso <shiriso1801@gmail.com>
2021-11-26 03:32:05 +0100

    add an exception which will be used for invalid source repository configurations

---
+++ src/Exceptions/MissingBasePathException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Shiriso\Kitsune\Core\Exceptions;
+
+use Exception;
+use Throwable;
+
+class MissingBasePathException extends Exception
+{
+    public function __construct($extraSourceAlias, $code = 0, Throwable $previous = null)
+    {
+        parent::__construct(sprintf('There was no valid base path configured for "%s".', $extraSourceAlias), $code, $previous);
+    }
+}
