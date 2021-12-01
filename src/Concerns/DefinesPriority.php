9845ce9c1c25d57b9fd38fe9ec1d3516a05fe2a3
Shiriso <shiriso1801@gmail.com>
2021-12-02 00:09:44 +0100

    add a concern and a class for usages of priority levels to sort source directories

---
+++ src/Concerns/DefinesPriority.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Shiriso\Kitsune\Core\Concerns;
+
+interface DefinesPriority
+{
+    /**
+     * Get the numeric representation of the priority.
+     *
+     * @return int
+     */
+    public function getPriorityValue(): int;
+
+    /**
+     * Get the numeric representation of the priority for cases it should be higher than the default.
+     * In Kitsune's context this is the case for paths which have been modified with the layout.
+     *
+     * @return int
+     */
+    public function getIncreasedPriorityValue(): int;
+}
---
+++ src/KitsunePriority.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Shiriso\Kitsune\Core;
+
+use Shiriso\Kitsune\Core\Concerns\DefinesPriority;
+use Shiriso\Kitsune\Core\Exceptions\InvalidPriorityException;
+
+class KitsunePriority implements DefinesPriority
+{
+    protected int $layoutIncrement = 5;
+    protected array $priorities = [
+        'least' => 10,
+        'low' => 20,
+        'medium' => 30,
+        'high' => 40,
+        'important' => 50,
+    ];
+    protected string $priority;
+
+    /**
+     * @throws InvalidPriorityException
+     */
+    public function __construct(string $priority = 'medium')
+    {
+        if (!array_key_exists($priority, $this->priorities)) {
+            throw new InvalidPriorityException($priority);
+        }
+
+        $this->priority = $priority;
+    }
+
+    public function getPriorityValue(): int
+    {
+        return $this->priorities[$this->priority];
+    }
+
+    public function getIncreasedPriorityValue(): int
+    {
+        return $this->getPriorityValue() + $this->layoutIncrement;
+    }
+}
