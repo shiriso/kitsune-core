755a9ebe0cf4246f2a85d7bf673673c83411b16e
Shiriso <shiriso1801@gmail.com>
2021-12-07 18:58:46 +0100

    refactor KitsuneMiddleware as setActiveLayout has been renamed and moved to another class

--- src/Middleware/KitsuneMiddleware.php
+++ src/Middleware/KitsuneMiddleware.php
@@ -3,15 +3,16 @@
 namespace Shiriso\Kitsune\Core\Middleware;
 
 use Closure;
+use Shiriso\Kitsune\Core\Concerns\UtilisesKitsune;
 
 class KitsuneMiddleware
 {
+    use UtilisesKitsune;
+
     public function handle($request, Closure $next, $layout = null)
     {
         if ($layout) {
-            app('kitsune')->setActiveLayout($layout);
-        } else {
-            app('kitsune')->refreshViewSources();
+            $this->getKitsuneManager()->setApplicationLayout($layout);
         }
 
         return $next($request);
