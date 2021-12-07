e3ea5ab904b850aefcde540c8c40bbcafab08c5d
Shiriso <shiriso1801@gmail.com>
2021-12-07 18:59:29 +0100

    add possibility to enable and define the global mode namespace using the same middleware

--- src/Middleware/KitsuneGlobalModeMiddleware.php
+++ src/Middleware/KitsuneGlobalModeMiddleware.php
@@ -3,15 +3,22 @@
 namespace Shiriso\Kitsune\Core\Middleware;
 
 use Closure;
+use Shiriso\Kitsune\Core\Concerns\UtilisesKitsune;
 
 class KitsuneGlobalModeMiddleware
 {
+    use UtilisesKitsune;
+
     public function handle($request, Closure $next, $state = true)
     {
         if (!$state || $state === 'false') {
-            app('kitsune')->disableGlobalMode();
+            $this->getKitsuneCore()->disableGlobalMode();
         } else {
-            app('kitsune')->enableGlobalMode();
+            $this->getKitsuneCore()->enableGlobalMode();
+
+            if (is_string($state) && $this->getKitsuneManager()->hasNamespace($state)) {
+                $this->getKitsuneCore()->setGlobalNamespace($state);
+            }
         }
 
         return $next($request);
